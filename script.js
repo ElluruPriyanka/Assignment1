5a2ff4479fe358380f6f9a69ecbbfcc8d4c4ef3d
Your Name <you@example.com>
2022-12-02 22:32:42 +0800

    update 6

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 
-// let slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
 
+sideNav.style.right ="-250px";
+
+menuBtn.onclick = function(){
+    if(sideNav.style.right =="-250px"){
+        sideNav.style.right= "0";
+    }
+    else{
+        sideNav.style.right ="-250px";
+    }
+}

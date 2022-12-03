e10c4ba1cc4ba00ce52fbf1a7f850fa302d9c088
Your Name <you@example.com>
2022-12-03 12:43:13 +0800

    update 7

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 var menuBtn = document.getElementById("menuBtn")
 var sideNav = document.getElementById("sideNav")
 var menu = document.getElementById("menu")
@@ -13,4 +12,29 @@ menuBtn.onclick = function(){
     else{
         sideNav.style.right ="-250px";
     }
+}
+// javacript gallery 
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+// slideshow for homepage 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
